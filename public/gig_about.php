839bf7069cb90506ad0b162e6be102170e9bf449
Daniel Brakke <brakphoto@gmail.com>
2019-10-20 07:33:33 -0500

    prevent clashes

--- public/gig_about.php
+++ public/gig_about.php
@@ -18,7 +18,7 @@
         <div class="gigs-table-item">
             <h4>Videos</h4>
             <?php foreach ($gig_videos as $j => $video) : ?>
-                <iframe src="<?php echo $video["video_url"] ?>" id="gigVideo<?php echo $j ?>">
+                <iframe src="<?php echo $video["video_url"] ?>" id="gig<?php echo $gig_id; ?>Video<?php echo $j ?>">
                 </iframe>
             <?php endforeach; ?>
         </div>
